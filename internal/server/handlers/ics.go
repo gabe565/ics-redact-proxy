91ac282675e211a2e40e534e2e5a3a0842baf943
Gabe Cook <gabe565@gmail.com>
2024-06-30 17:06:45 -0500

    chore(server): Add no-cache middleware

--- internal/server/handlers/ics.go
+++ internal/server/handlers/ics.go
@@ -58,8 +58,6 @@ func ICS(conf *config.Config) http.HandlerFunc {
 		}
 
 		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-
 		if err := cal.SerializeTo(w); err != nil {
 			log.Err(err).Msg("Failed to write ics")
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
