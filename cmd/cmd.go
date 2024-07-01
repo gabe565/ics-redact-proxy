b542d319861f84c0c299e6ab90619d8d46766a13
Gabe Cook <gabe565@gmail.com>
2024-06-30 22:05:25 -0500

    chore(cmd): Log version on startup

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,17 +8,20 @@ import (
 
 	"github.com/gabe565/ics-availability-server/internal/config"
 	"github.com/gabe565/ics-availability-server/internal/server"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 var version = "beta"
 
 func New() *cobra.Command {
+	version, commit := buildVersion(version)
 	cmd := &cobra.Command{
-		Use:     "ics-availability-server",
-		Short:   "Fetches an ics file and redacts all data except for configured fields.",
-		RunE:    run,
-		Version: buildVersion(version),
+		Use:         "ics-availability-server",
+		Short:       "Fetches an ics file and redacts all data except for configured fields.",
+		RunE:        run,
+		Version:     version,
+		Annotations: map[string]string{"commit": commit},
 
 		DisableAutoGenTag: true,
 	}
@@ -40,6 +43,8 @@ func run(cmd *cobra.Command, _ []string) error {
 	if err := conf.Load(cmd); err != nil {
 		return err
 	}
+
+	log.Info().Str("version", version).Str("commit", cmd.Annotations["commit"]).Msg("ICS availability server")
 
 	ctx, cancel := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	defer cancel()
