7e44b1e33550869cfeb912d95aae0970c1e305f2
Gabe Cook <gabe565@gmail.com>
2024-12-01 01:24:10 -0600

    chore(config): Rename flag consts

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -11,15 +11,15 @@ const (
 	FlagLogLevel  = "log-level"
 	FlagLogFormat = "log-format"
 
-	ListenAddressFlag = "listen-address"
-	TokenFlag         = "token"
-	RealIPHeaderFlag  = "real-ip-header"
+	FlagListenAddress = "listen-address"
+	FlagToken         = "token"
+	FlagRealIPHeader  = "real-ip-header"
 
-	SourceURLFlag        = "source-url"
-	EventAllowFieldsFlag = "event-allow-fields"
-	NewCalendarNameFlag  = "new-calendar-name"
-	NewEventSummaryFlag  = "new-event-summary"
-	HashUIDFlag          = "hash-uid"
+	FlagSourceURL        = "source-url"
+	FlagEventAllowFields = "event-allow-fields"
+	FlagNewCalendarName  = "new-calendar-name"
+	FlagNewEventSummary  = "new-event-summary"
+	FlagHashUID          = "hash-uid"
 )
 
 func (c *Config) RegisterFlags(f *pflag.FlagSet) {
@@ -28,14 +28,14 @@ func (c *Config) RegisterFlags(f *pflag.FlagSet) {
 
 	f.BoolVar(&c.NoVerify, "no-verify", c.NoVerify, "Skips source verification request on startup")
 	f.StringVar(&c.ListenAddress, "addr", c.ListenAddress, "Listen address")
-	must.Must(f.MarkDeprecated("addr", "use --"+ListenAddressFlag+" instead"))
-	f.StringVar(&c.ListenAddress, ListenAddressFlag, c.ListenAddress, "Listen address")
-	f.StringSliceVar(&c.Tokens, TokenFlag, c.Tokens, "Enables token auth (requests will require a `token` GET parameter)")
-	f.BoolVar(&c.RealIPHeader, RealIPHeaderFlag, c.RealIPHeader, `Get client IP address from the "Real-IP" header`)
+	must.Must(f.MarkDeprecated("addr", "use --"+FlagListenAddress+" instead"))
+	f.StringVar(&c.ListenAddress, FlagListenAddress, c.ListenAddress, "Listen address")
+	f.StringSliceVar(&c.Tokens, FlagToken, c.Tokens, "Enables token auth (requests will require a `token` GET parameter)")
+	f.BoolVar(&c.RealIPHeader, FlagRealIPHeader, c.RealIPHeader, `Get client IP address from the "Real-IP" header`)
 
-	f.StringVar(&c.SourceURL, SourceURLFlag, c.SourceURL, "Source iCal URL")
-	f.StringSliceVar(&c.EventAllowFields, EventAllowFieldsFlag, c.EventAllowFields, "Allowed event fields")
-	f.StringVar(&c.NewCalendarName, NewCalendarNameFlag, c.NewCalendarName, "If set, calendar name will be changed to this value")
-	f.StringVar(&c.NewEventSummary, NewEventSummaryFlag, c.NewEventSummary, "If set, event summaries will be changed to this value")
-	f.BoolVar(&c.HashUID, HashUIDFlag, c.HashUID, "Replace event UID with a hash. The UID can leak domains and IP addresses so this option is recommended.")
+	f.StringVar(&c.SourceURL, FlagSourceURL, c.SourceURL, "Source iCal URL")
+	f.StringSliceVar(&c.EventAllowFields, FlagEventAllowFields, c.EventAllowFields, "Allowed event fields")
+	f.StringVar(&c.NewCalendarName, FlagNewCalendarName, c.NewCalendarName, "If set, calendar name will be changed to this value")
+	f.StringVar(&c.NewEventSummary, FlagNewEventSummary, c.NewEventSummary, "If set, event summaries will be changed to this value")
+	f.BoolVar(&c.HashUID, FlagHashUID, c.HashUID, "Replace event UID with a hash. The UID can leak domains and IP addresses so this option is recommended.")
 }
