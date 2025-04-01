d1b8459e604b2d9001aa1ddb3675a61ee1a55e6e
Gabe Cook <gabe565@gmail.com>
2025-04-01 02:20:58 -0500

    test(lint): update lint config to v2, add linters, fix warnings

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -31,14 +31,26 @@ func (c *Config) RegisterFlags(f *pflag.FlagSet) {
 
 	f.BoolVar(&c.NoVerify, FlagNoVerify, c.NoVerify, "Skips source verification request on startup")
 	f.StringVar(&c.ListenAddress, FlagListenAddress, c.ListenAddress, "Listen address")
-	f.StringSliceVar(&c.Tokens, FlagToken, c.Tokens, "Enables token auth (requests will require a `token` GET parameter)")
+	f.StringSliceVar(&c.Tokens, FlagToken, c.Tokens,
+		"Enables token auth (requests will require a `token` GET parameter)",
+	)
 	f.BoolVar(&c.RealIPHeader, FlagRealIPHeader, c.RealIPHeader, `Get client IP address from the "Real-IP" header`)
-	f.IntVar(&c.RateLimitMaxRequests, FlagRateLimitMaxRequests, c.RateLimitMaxRequests, "Rate limiter max requests per IP")
-	f.DurationVar(&c.RateLimitInterval, FlagRateLimitInterval, c.RateLimitInterval, "Rate limiter sliding window interval")
+	f.IntVar(&c.RateLimitMaxRequests, FlagRateLimitMaxRequests, c.RateLimitMaxRequests,
+		"Rate limiter max requests per IP",
+	)
+	f.DurationVar(&c.RateLimitInterval, FlagRateLimitInterval, c.RateLimitInterval,
+		"Rate limiter sliding window interval",
+	)
 
 	f.StringVar(&c.SourceURL, FlagSourceURL, c.SourceURL, "Source iCal URL")
 	f.StringSliceVar(&c.EventAllowFields, FlagEventAllowFields, c.EventAllowFields, "Allowed event fields")
-	f.StringVar(&c.NewCalendarName, FlagNewCalendarName, c.NewCalendarName, "If set, calendar name will be changed to this value")
-	f.StringVar(&c.NewEventSummary, FlagNewEventSummary, c.NewEventSummary, "If set, event summaries will be changed to this value")
-	f.BoolVar(&c.HashUID, FlagHashUID, c.HashUID, "Replace event UID with a hash. The UID can leak domains and IP addresses so this option is recommended.")
+	f.StringVar(&c.NewCalendarName, FlagNewCalendarName, c.NewCalendarName,
+		"If set, calendar name will be changed to this value",
+	)
+	f.StringVar(&c.NewEventSummary, FlagNewEventSummary, c.NewEventSummary,
+		"If set, event summaries will be changed to this value",
+	)
+	f.BoolVar(&c.HashUID, FlagHashUID, c.HashUID,
+		"Replace event UID with a hash. The UID can leak domains and IP addresses so this option is recommended.",
+	)
 }
